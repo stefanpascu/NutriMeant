70f27930cbeb47568ea0f6ebfac9148d74ddcfe2
Ioana Pascu <ioana.pascu9596@yahoo.com>
2018-07-14 18:48:36 +0300

    modify goal activity layout

--- app/src/main/java/myfirst/app/Activity2.java
+++ app/src/main/java/myfirst/app/Activity2.java
@@ -3,53 +3,17 @@ package myfirst.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ImageButton;
 
 import maes.tech.intentanim.CustomIntent;
 
 public class Activity2 extends AppCompatActivity {
-    private ImageButton imagebutton;
-    private ImageButton imagebutton2;
-    private ImageButton imagebutton3;
-    private ImageButton imagebutton4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
-        imagebutton = (ImageButton) findViewById(R.id.imageButton);
-        imagebutton2 = (ImageButton) findViewById(R.id.imageButton2);
-        imagebutton3 = (ImageButton) findViewById(R.id.imageButton3);
-        imagebutton4 = (ImageButton) findViewById(R.id.imageButton4);
-        imagebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFatUp();
-            }
-        });
-
-        imagebutton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFatUp();
-            }
-        });
-
-        imagebutton3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFatUp();
-            }
-        });
-
-        imagebutton4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFatUp();
-            }
-        });
     }
 
     @Override
